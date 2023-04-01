c027dad355ee9844e6b53e8008170f4899e1aefe
JBattishill <jbattishill91@gmail.com>
2023-04-01 18:07:34 +1100

    Functional players now - logo uploaded

--- frontend/components/Spotify.vue
+++ frontend/components/Spotify.vue
@@ -1,20 +1,6 @@
 <template>
-    <div>
-
-        <div class="context uk-section uk-margin-auto uk-width-3-4 uk-flex uk-flex-center">
-            <div class="uk-width-1-2">
-                <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/4dtBz6G3OxmXcQIsulJbAI?utm_source=generator" 
-                width="80%" height="600" frameBorder="0" allowfullscreen="false" 
-                allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-            </div>
-
-            <div class="uk-width-1-2">
-                <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/3NgUACqzz04eVZFiiAx0GR?utm_source=generator" 
-                width="80%" height="600" frameBorder="0" allowfullscreen="false" 
-                allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-            </div>
-        </div>
-
+    <div class="area">
+        <div class="uk-section uk-margin-auto uk-width-3-4 uk-flex uk-flex-center">
             <div class="area">
             <div class="circles">
                     <i class="star" aria-hidden="true"><img src="../static/star.png"></i>
@@ -40,6 +26,21 @@
                     <i class="star" aria-hidden="true"><img src="../static/star.png"></i>
                     <i class="star" aria-hidden="true"><img src="../static/star.png"></i>
             </div>
+            <div class="uk-flex uk-flex-center">
+                <a class="uk-flex" href="/"><img class="toplogo uk-width-3-4@s uk-width-1-2@m uk-width-3-4@l" src="../../frontend/static/stsLogo.png" alt="Choose Nourish Logo."/></a>
+            </div>
+            <div class="uk-width-1-2">
+                <iframe class="context" style="border-radius:12px" src="https://open.spotify.com/embed/playlist/3NgUACqzz04eVZFiiAx0GR?utm_source=generator" 
+                width="80%" height="600" frameBorder="0" allowfullscreen="false" 
+                allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+            </div>
+          
+            <div class="uk-width-1-2">
+                <iframe class="contextright" style="border-radius:12px" src="https://open.spotify.com/embed/playlist/4dtBz6G3OxmXcQIsulJbAI?utm_source=generator" 
+                width="80%" height="600" frameBorder="0" allowfullscreen="false" 
+                allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+            </div>
+        </div>
         </div>
     </div>
 </template>
